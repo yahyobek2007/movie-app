809adcc8b7209d91172e4edf408ca90edfc019e7
yahyobek2007 <yahyobek2150@gmail.com>
2023-05-19 15:39:31 +0500

    intion commit

---
+++ js/uyga.js
@@ -0,0 +1,29 @@
+const elList = document.getElementById('list')
+
+fetch('https://www.omdbapi.com/?apikey=72ec5480&s=thor')
+    .then((response) => response.json())
+    .then((data) => getFunction(data.Search))
+
+function getFunction(item) {
+    console.log(item)
+
+
+    item.forEach((inner, index) => {
+        const elItem = document.createElement('li')
+        const elImg = document.createElement('img')
+        const elTitle = document.createElement('h3')
+        const elType = document.createElement('h4')
+        const elYear = document.createElement('p')
+
+        elImg.src = inner.Poster
+        elTitle.textContent = inner.Title
+        elType.textContent = inner.Type
+        elYear.textContent = inner.Year
+
+        elList.append(elItem)
+        elItem.append(elImg)
+        elItem.append(elTitle)
+        elItem.append(elType)
+        elItem.append(elYear)
+    })
+}
